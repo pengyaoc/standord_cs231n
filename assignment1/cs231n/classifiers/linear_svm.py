f51496974a7a1cb6c2f1b0f90df74e7b92f69756
Pengyao Chen <pengyaoc@gmail.com>
2017-08-04 19:57:57 -0700

    [SVM] update linear_svm.py to implement vectorized svm loss and gradient

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,10 +35,14 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,y[i]] -= X[i] # cost func derived by Wyi = -Xi
+        dW[:,j] += X[i] # cost function derived by Wj = Xi
+
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
@@ -70,7 +74,15 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores_over_all_sample = X.dot(W)
+  # https://docs.scipy.org/doc/numpy/user/basics.indexing.html
+  correct_class_score_over_all_sample = scores_over_all_sample[np.arange(y.shape[0]), y]
+  # transpose of correct_class_score_over_all_sample
+  correct_class_score_over_all_sample_T = correct_class_score_over_all_sample.reshape(-1, 1)
+  # calculate margin
+  margin_over_all_sample = scores_over_all_sample - correct_class_score_over_all_sample_T + 1
+  margin_over_all_sample_positive = np.maximum(margin_over_all_sample, 0)
+  loss = np.sum(margin_over_all_sample_positive)/y.shape[0] - 1
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -85,7 +97,14 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  mask = np.zeros(margin_over_all_sample_positive.shape)
+  # all positive margin except for the right class would be plus Xi
+  mask[margin_over_all_sample_positive > 0] = 1
+  # all right-class margin would be -Xi * (num of positive margins)
+  # for the +1 here: margin for the expected class is be calculated
+  # as positive margin 1 which should be zero
+  mask[np.arange(y.shape[0]), y] = -np.sum(mask, axis=1) + 1
+  dW = X.T.dot(mask)/y.shape[0]
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
