add9b055cb504439c6bfbca5e0ffb9796d0186de
Pengyao Chen <pengyaoc@gmail.com>
2017-08-08 23:27:54 -0700

    [FC Net] Implement FC/reLU forward and backward pass and Two Layer Net

--- assignment2/cs231n/classifiers/fc_net.py
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -47,7 +47,16 @@ class TwoLayerNet(object):
         # weights and biases using the keys 'W1' and 'b1' and second layer weights #
         # and biases using the keys 'W2' and 'b2'.                                 #
         ############################################################################
-        pass
+        self.params['W1'] = np.random.normal(
+            size=(input_dim, hidden_dim),
+            scale=weight_scale,
+        )
+        self.params['b1'] = np.zeros(hidden_dim)
+        self.params['W2'] = np.random.normal(
+            size=(hidden_dim, num_classes),
+            scale=weight_scale,
+        )
+        self.params['b2'] = np.zeros(num_classes)
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -77,7 +86,8 @@ class TwoLayerNet(object):
         # TODO: Implement the forward pass for the two-layer net, computing the    #
         # class scores for X and storing them in the scores variable.              #
         ############################################################################
-        pass
+        fc1, cache_fc1 = affine_relu_forward(X, self.params['W1'], self.params['b1'])
+        scores, cache_fc2 = affine_forward(fc1, self.params['W2'], self.params['b2'])
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
@@ -97,7 +107,17 @@ class TwoLayerNet(object):
         # automated tests, make sure that your L2 regularization includes a factor #
         # of 0.5 to simplify the expression for the gradient.                      #
         ############################################################################
-        pass
+        loss, dx = softmax_loss(scores, y)
+        loss += 0.5 * self.reg * (
+            np.sum(self.params['W1'] * self.params['W1']) + 
+            np.sum(self.params['W2'] * self.params['W2'])
+        )
+        dx, dw, db = affine_backward(dx, cache_fc2)
+        grads['W2'] = dw + self.reg * self.params['W2']
+        grads['b2'] = db
+        dx, dw, db = affine_relu_backward(dx, cache_fc1)
+        grads['W1'] = dw + self.reg * self.params['W1']
+        grads['b1'] = db
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
